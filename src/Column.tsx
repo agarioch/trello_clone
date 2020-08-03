3692ca090aab66151be76e258656a36d26b1a9aa
agarioch <alistairgarioch@gmail.com>
2020-08-03 22:14:55 +0100

    Implement column useDrop and update styles to decrease opacity when column is being dragged

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,9 +1,12 @@
 import React, { useRef } from "react";
+import { useDrop } from "react-dnd";
 import { ColumnContainer, ColumnTitle } from "./styles";
 import { AddNewItem } from "./AddNewItem";
 import { useAppState } from "./AppStateContext";
 import { Card } from "./Card";
 import { useItemDrag } from "./useItemDrag";
+import { DragItem } from "./DragItem";
+import { isHidden } from "./utils/isHidden";
 
 interface ColumnProps {
   text: string;
@@ -12,15 +15,32 @@ interface ColumnProps {
 }
 
 export const Column = ({ text, index, id }: ColumnProps) => {
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover(item: DragItem) {
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+
+      dispatch({ type: "MOVE_LIST", payload: { dragIndex, hoverIndex } });
+      item.index = hoverIndex;
+    },
+  });
+
   const { state, dispatch } = useAppState();
   const ref = useRef<HTMLDivElement>(null);
 
   const { drag } = useItemDrag({ type: "COLUMN", id, index, text });
 
-  drag(ref);
+  drag(drop(ref));
 
   return (
-    <ColumnContainer ref={ref}>
+    <ColumnContainer
+      ref={ref}
+      isHidden={isHidden(state.draggedItem, "COLUMN", id)}
+    >
       <ColumnTitle>{text}</ColumnTitle>
       {state.lists[index].tasks.map((task, i) => (
         <Card text={task.text} key={task.id} index={i} />
